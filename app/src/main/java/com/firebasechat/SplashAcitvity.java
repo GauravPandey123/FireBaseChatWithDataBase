61bbe68918b2f49d8c8479374300522cbcb81ccb
Gaurav Pandey <gpandey@synapseco.com>
2016-12-30 15:15:08 +0530

    FireBaseChat

---
+++ app/src/main/java/com/firebasechat/SplashAcitvity.java
@@ -0,0 +1,19 @@
+package com.firebasechat;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gpandey on 12/30/2016.
+ */
+
+public class SplashAcitvity extends AppCompatActivity {
+
+
+  @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_intro_screen);
+    }
+}
